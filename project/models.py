d67dddae0efb43ac55226239b14fb18de83c0f89
Clemo97 <lumumbaclement@gmail.com>
2022-06-14 19:23:44 +0300

    class Profile definition

--- project/models.py
+++ project/models.py
@@ -3,3 +3,13 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from tinymce.models import HTMLField
+
+
+class Profile(models.Model):
+    profile_pic = models.ImageField( upload_to='profile/', blank ='true',default='default.png')
+    bio = models.TextField()
+    user =models.OneToOneField(User, on_delete = models.CASCADE)
+    date_craeted= models.DateField(auto_now_add=True )
+
+
+    
