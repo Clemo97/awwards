5659636d29acc827b29ef3766fba46801d67ea73
Clemo97 <lumumbaclement@gmail.com>
2022-06-14 20:16:59 +0300

    import needed for views

--- project/views.py
+++ project/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .forms import Project, UserUpdateForm, ProfileUpdateForm, SignUpForm, NewProjectForm
+from django.contrib import messages
+from .models import Profile, Project
+from rest_framework import status
 
-# Create your views here.
+
+@login_required(login)
