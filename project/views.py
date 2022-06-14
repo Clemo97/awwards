8b892a09d13501ee8b6b4d216267f08c5c1dc39f
Clemo97 <lumumbaclement@gmail.com>
2022-06-14 20:25:06 +0300

    login required function decorator

--- project/views.py
+++ project/views.py
@@ -9,4 +9,27 @@ from .models import Profile, Project
 from rest_framework import status
 
 
-@login_required(login)
+@login_required(login_url='/accounts/login/')
+def home(request):
+    project= Project.all_projects()
+    json_projects = []
+    for project in project:
+
+        pic = Profile.objects.filter(user=project.user.id).first()
+        if pic:
+            pic = pic.profile_pic.url
+        else:
+            pic =''
+        obj = dict(
+            title=project.title,
+            image=project.image,
+            link=project.link,
+            description=project.description,
+            avatar=pic,
+            date_craeted=project.date_craeted,
+            author=project.user.username  
+        )
+        json_projects.append(obj)
+        # print(json_projects)
+    
+    return render(request, 'home.html', {"json_projects": json_projects})
