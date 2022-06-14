481fe86cff575f364bbac76e879ee135b8229599
Clemo97 <lumumbaclement@gmail.com>
2022-06-15 00:00:12 +0300

    final urls path

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
-from project import views as user_views
+from rates import views as user_views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
     path('',views.home,name = 'home'),
+    path('accounts/register/', views.register, name='register'),
+    path('profile/', views.profile,name = 'profile'),
+    path('update_profile/', user_views.update_profile,name = 'update_profile'),
+    path('new_project/', views.new_project,name ='new_project'),
+    path('search/', views.search_results, name = 'search_results'),
 ]
 
 
