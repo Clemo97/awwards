cd7bf702a3168ac27e6ee08c9701659d4467c012
Clemo97 <lumumbaclement@gmail.com>
2022-06-14 18:42:28 +0300

    add projects app to settings.py

--- awward/settings.py
+++ awward/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'project',
+    'bootstrap4',
+    'crispy_forms',
+    'tinymce',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'awward.urls'
@@ -62,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -73,12 +80,28 @@ WSGI_APPLICATION = 'awward.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config('MODE')=='dev':
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER':config("DB_USER"),
+            'PASSWORD':config('DB_PASSWORD'),
+            'HOST':config("DB_HOST")
+        }
     }
-}
+
+else:
+    DATABASES={
+    'default':dj_database_url.config(
+    default=config('DATABASE_URL')
+    )
+    }
+
+db_from_evn=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_evn)
+ALLOWED_HOST=config('ALLOWED_HOSTS',cast=Csv)
 
 
 # Password validation
@@ -105,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
